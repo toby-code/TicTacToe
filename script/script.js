3097d4d3bf1e22016f1f7b3a1d6faffccb7ede9c
toby-code <mueller.to133@gmail.com>
2021-02-11 17:02:45 +0100

    Updated html code and js

---
+++ script/script.js
@@ -0,0 +1,13 @@
+let fields = [];
+let currentShape = 'cross';
+
+
+function fillShape(id) {
+    if(currentShape == 'cross') {
+        currentShape = 'circle';
+    } else {
+        currentShape = 'cross';
+    }
+    fields[id] = currentShape;
+    console.log(fields);
+}
